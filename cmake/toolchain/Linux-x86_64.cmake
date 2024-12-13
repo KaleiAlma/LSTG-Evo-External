9d4e9906dc31b4e42c7cbede94f2db22489db8ea
Kalei Alma <kalei@tuta.io>
2024-12-13 08:49:11 -0700

    let's hope this works

--- cmake/toolchain/Linux-x86_64.cmake
+++ cmake/toolchain/Linux-x86_64.cmake
@@ -5,4 +5,5 @@ set(CMAKE_C_COMPILER clang-18)
 set(CMAKE_C_COMPILER_TARGET x86_64-pc-linux-gnu)
 set(CMAKE_CXX_COMPILER clang++-18)
 set(CMAKE_CXX_COMPILER_TARGET x86_64-pc-linux-gnu)
+
 set(CMAKE_LINKER_TYPE LLD)
