60364276f3f5412b76535674a737064bae5ecb8e
Kalei Alma <kalei@tuta.io>
2024-12-12 18:49:43 -0700

    guess i'm supporting arm now

---
+++ cmake/toolchain/Linux-arm64.cmake
@@ -0,0 +1,7 @@
+
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER clang-18)
+set(CMAKE_C_COMPILER_TARGET aarch64-pc-linux-gnu)
+set(CMAKE_CXX_COMPILER clang++-18)
+set(CMAKE_CXX_COMPILER_TARGET aarch64-pc-linux-gnu)
---
+++ cmake/toolchain/macOS-x86_64.cmake
@@ -0,0 +1,13 @@
+
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE INTERNAL "")
+
+execute_process(
+    COMMAND brew --prefix llvm@18
+    OUTPUT_VARIABLE LLVM_DIR
+)
+
+set(CMAKE_C_COMPILER ${LLVM_DIR}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET x86_64-apple-darwin)
+set(CMAKE_CXX_COMPILER ${LLVM_DIR}/bin/clang++)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-apple-darwin)
