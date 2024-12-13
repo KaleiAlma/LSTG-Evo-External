949d1b462074bf16b4625de1589635dafbd2b92c
Kalei Alma <kalei@tuta.io>
2024-12-12 20:14:29 -0700

    even more fixes

--- cmake/toolchain/Linux-arm64.cmake
+++ cmake/toolchain/Linux-arm64.cmake
@@ -5,3 +5,5 @@ set(CMAKE_C_COMPILER clang-18)
 set(CMAKE_C_COMPILER_TARGET aarch64-pc-linux-gnu)
 set(CMAKE_CXX_COMPILER clang++-18)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-pc-linux-gnu)
+
+set(CMAKE_LINKER_TYPE LLD)
