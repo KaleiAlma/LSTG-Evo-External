c26f4d3d695d7fe5dcb2bb3304a8e1f29c6a2b1f
Kalei Alma <kalei@tuta.io>
2025-03-07 10:17:23 -0700

    fix minizip

--- cmake/packages.z.cmake
+++ cmake/packages.z.cmake
@@ -1,10 +1,11 @@
+set(ZSTD_TAG v1.5.6 CACHE STRING "")
+
 # minizip
 # Load zip files
 
 
 CPMAddPackage(
     NAME minizip_ng
-    VERSION 4.0.5
     GITHUB_REPOSITORY zlib-ng/minizip-ng
     GIT_TAG 4.0.5
     OPTIONS
@@ -13,7 +14,6 @@ CPMAddPackage(
         "WITH_GTEST OFF"
         "ZLIB_REPOSITORY https://github.com/zlib-ng/zlib-ng"
         "ZLIB_TAG 2.1.6"
-        "ZSTD_TAG v1.5.6"
 )
 
 # thankfully i figured out how to make zlib-ng/minizip-ng play nice ig
