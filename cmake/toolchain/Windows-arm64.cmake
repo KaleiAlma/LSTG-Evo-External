62dd90deed0f66fc5028e41b95da0d1c9fa8597e
Kalei Alma <kalei@tuta.io>
2025-03-08 11:59:53 -0700

    fix windows, change equal to strequal

--- cmake/toolchain/Windows-arm64.cmake
+++ cmake/toolchain/Windows-arm64.cmake
@@ -4,3 +4,8 @@ set(CMAKE_SYSTEM_NAME Windows)
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
+
+if(NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64")
+    set(CMAKE_C_COMPILER_TARGET aarch64-windows-msvc)
+    set(CMAKE_CXX_COMPILER_TARGET aarch64-windows-msvc)
+endif()
